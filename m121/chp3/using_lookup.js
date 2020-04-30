745cf7c546d75481487fb2fadb2c620f5b45210e
nuistzhou <nuistzhou@gmail.com>
2020-04-30 17:07:22 +0200

    Update using_lookup.js

--- m121/chp3/using_lookup.js
+++ m121/chp3/using_lookup.js
@@ -1,1 +1,22 @@
+// Builds the pipeline.
+var pipeline = [
+	{$unwind: "$airlines"},
+	{$lookup: 
+		{
+			from: "air_routes",
+			localField: "airlines",
+			foreignField: "airline.name",
+			as: "routes"
+		}},
+	{$unwind: "$routes"},	
+	{$match: {"routes.airplane": {$in: ["747", "380"]}}},
+	{$group: {
+			"_id": "$name",
+			"routes_count": {$sum: 1}
+	}},
+	{$sort: {"routes_count": -1}}
+];
 
+// Prints the result.
+print("Result: ");
+printjson(db.air_alliances.aggregate(pipeline).next(););
